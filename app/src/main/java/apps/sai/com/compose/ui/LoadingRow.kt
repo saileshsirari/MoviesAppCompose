d81d0d3331d44dbae5d276f99c3939a403cc792a
saileshsirari <sailesh.sirari@gmail.com>
2024-08-08 18:18:26 +0530

    Update LoadingRow.kt

--- app/src/main/java/apps/sai/com/compose/ui/LoadingRow.kt
+++ app/src/main/java/apps/sai/com/compose/ui/LoadingRow.kt
@@ -3,9 +3,12 @@ package apps.sai.com.compose.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,12 +24,16 @@ fun LoadingRow(modifier: Modifier = Modifier) {
         horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        CircularProgressIndicator(modifier = Modifier.size(40.dp))
+        CircularProgressIndicator(
+            modifier = Modifier.width(64.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
     }
 }
 
 @Preview(showSystemUi = true)
 @Composable
 fun LoadingRowPreview() {
-    LoadingRow()
+    LoadingRow(modifier =  Modifier)
 }
